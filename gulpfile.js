d207fd265eae3900bf1cafae6d176dab1f0697c2
Derek Rudel <derek@rudelcreative.com>
2020-06-23 12:27:35 -0600

    installed gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCss = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
 
-sass.compiler = require('node-sass');
+sass.compiler = require('node-sass')
 
 gulp.task("sass", function () {
     return gulp.src("css/app.scss")
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(
+            cleanCss({
+                compatability: 'ie8'
+            })
+        )
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("."))
-});
+})
 
 gulp.task("watch", function () {
     gulp.watch("css/app.scss", gulp.series("sass"))
-});
+})
 
-gulp.task('default', gulp.series("sass", "watch"));
+gulp.task('default', gulp.series("sass", "watch"))
