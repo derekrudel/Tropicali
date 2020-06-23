c2c125b8c5b0520feca549e5312fef805c51dcdf
Derek Rudel <derek@rudelcreative.com>
2020-06-23 10:02:40 -0600

    got project up to speed with video 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-<<<<<<< HEAD
 var sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
@@ -7,11 +6,5 @@ sass.compiler = require('node-sass');
 gulp.task("default", function () {
     return gulp.src("css/app.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app.css"))
+        .pipe(gulp.dest("."))
 })
-=======
-
-gulp.task('default', function () {
-    // place code for your default task here
-});
->>>>>>> 3bda1a2d8bf76c288f3bb7e9cfa6857d79283f27
